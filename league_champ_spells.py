9d661d3c2ec86a1b47384c3fcb80fc1f3bcc999f
Andrew Law <33298177+andrewlawhh@users.noreply.github.com>
2018-06-06 22:29:20 -0700

    Add files via upload
    Fixed formatting, added documentation, removed boolean function

--- league_champ_spells.py
+++ league_champ_spells.py
@@ -1,20 +1,21 @@
 import cassiopeia as cass
+
+# SET API KEY
 cass.set_riot_api_key("RGAPI-34c1598d-d227-4396-9847-1bd4da688492")
+
+# SET REGION
 cass.set_default_region('NA')
+
+# CREATE CHAMPION DICTIONARY
+# KEY : String of champion name in lower case
+# VALUE : Champion object from Champion.gg API
+
 champions = cass.get_champions()
-
 champ_dict = {}
-
-for champ in champions: 
+for champ in champions:
 	champ_dict[champ.name.lower()] = champ
 
-def contains_champ(comment):
-	lst = comment.lower().split()
-	for word in lst:
-		if word in champ_dict:
-			return True
-	return False
-
+# GIVEN A STRING, RETURN A LIST OF ALL CHAMPION NAMES CONTAINED IN THE STRING
 def champ_list(comment):
 	lst = comment.lower().split()
 	result = []
@@ -23,6 +24,7 @@ def champ_list(comment):
 			result.append(word)
 	return result
 
+# GIVEN A STRING CHAMP NAME, RETURN A STRING CONTAINING ITS INFORMATION (PASSIVE + SPELLS)
 def champ_spells(champion_name):
 	champ = champ_dict[champion_name]
 	passive = champ.passive
@@ -32,6 +34,8 @@ def champ_spells(champion_name):
 	e = spells[2]
 	r = spells[3]
 
+	# TAKES A LIST AND RETURNS A LIST CONTAINING THE STR() OF EACH ELEMENT IN THE LIST
+	# USED TO MAKE THE COSTS AND COOLDOWNS PARSABLE AS STRINGS
 	def stringify_list(lst):
 		return [str(x) for x in lst]
 
@@ -44,15 +48,19 @@ def champ_spells(champion_name):
 	e_cd = stringify_list(e.cooldowns) 
 	r_cd = stringify_list(r.cooldowns) 
 
-	text = ''' Passive : {0} - {1}
-		   Q : {2}	Cost: {3}	Cooldown: {4}
-		   {5}
-		   W : {6}	Cost: {7}	Cooldown: {8}
-		   {9}
-		   E : {10}	Cost: {11}	Cooldown: {12}
-		   {13}
-		   R : {14}	Cost: {15}	Cooldown: {16}
-		   {17} 
+	text = '''    Passive : {0} - {1}
+	------------------------------------------------------
+	Q : {2}	Cost: {3}	Cooldown: {4}
+	{5}
+	------------------------------------------------------
+	W : {6}	Cost: {7}	Cooldown: {8}
+	{9}
+	------------------------------------------------------
+	E : {10}	Cost: {11}	Cooldown: {12}
+	{13}
+	------------------------------------------------------
+	R : {14}	Cost: {15}	Cooldown: {16}
+	{17} 
 	'''.format(passive.name, passive.description,
 				q.name, '/'.join(q_costs), '/'.join(q_cd), q.description,
 				w.name, '/'.join(w_costs), '/'.join(w_cd), w.description,
@@ -61,8 +69,10 @@ def champ_spells(champion_name):
 		)
 	return text
 
-def test_champ_spells():
-        print(champ_spells('ashe'))
+def test():
+	string = "I can't believe Singed is so broken right now. What even is that champ????? kappa kappa xd"
+	for champ in champ_list(string):
+		print(champ_spells(champ))
 
 if __name__ == '__main__':
-        test_champ_spells()
+	test()
